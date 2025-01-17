efcd7d4e4490197b24dd173d417c32fd69e50efe
csarte <csarte250@gmail.com>
2025-01-17 13:54:07 -0500

    feb17exercise

---
+++ C Programs/Feb17Countdown.c
@@ -0,0 +1,11 @@
+#include  <stdio.h>
+void main(int argc, char *argv[])
+{
+    int time = 10;
+    while(time > 0){
+        printf("%d\n", time);
+        time--;
+    }
+
+    printf("Happy New Year!");
+}
