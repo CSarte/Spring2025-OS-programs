096b06b3e3d080efa77e8e9255a9d2376f69eb19
csarte <csarte250@gmail.com>
2025-02-03 13:47:39 -0500

    OS work

---
+++ Operating Systems/5.2UsingWait.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int
+main(int argc, char *argv[])
+{
+    
+    int rc = fork();
+    if (rc < 0) {
+        // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        // child (new process)
+        printf("hello\n");
+	sleep(1);
+    } else {
+        // parent goes down this path (original process)
+        int wc = wait(NULL);
+        printf("goodbye\n");
+    }
+    return 0;
+}
